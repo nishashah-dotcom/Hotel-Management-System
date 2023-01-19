1934b19f726b35cb6c6ca61bea0ea26d57438183
nishashah-dotcom <54577721+nishashah-dotcom@users.noreply.github.com>
2023-01-19 22:06:13 +0530

    Create ServletInitializer.java

---
+++ src/main/java/org/training/ServletInitializer.java
@@ -0,0 +1,10 @@
+package org.training;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+public class ServletInitializer extends SpringBootServletInitializer{
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+       return application.sources(HotelManagementApplication.class);
+   }
+}
